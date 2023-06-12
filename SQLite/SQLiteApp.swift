e7facbf5cf0a7cee9ac7a29c7755e1ec57eea056
DylanMartin198 <111450540+DylanMartin198@users.noreply.github.com>
2023-06-12 08:15:58 -0600

    Initial commit

---
+++ SQLite/SQLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLiteApp.swift
+//  SQLite
+//
+//  Created by Dylan Martin on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SQLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
